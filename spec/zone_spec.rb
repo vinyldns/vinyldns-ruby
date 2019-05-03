b576d97b564f31f31f350f406f6adf088c1c6577
Britney Wright <britney_wright@comcast.com>
2019-05-03 10:53:42 -0400

    rename record

--- spec/zone_spec.rb
+++ spec/zone_spec.rb
@@ -110,7 +110,7 @@ describe Vinyldns::API::Zone::RecordSet do
   end
 
   it 'creates a new record with owner group ID' do
-    request = Vinyldns::API::Zone::RecordSet.create(first_zone['id'], 'testrubyrecordcreate', 'A', 200, [{'address': '1.1.1.1'}], first_group['id'])
+    request = Vinyldns::API::Zone::RecordSet.create(first_zone['id'], 'recordcreatewithowner', 'A', 200, [{'address': '1.1.1.1'}], first_group['id'])
     wait_until_recordset_active(first_zone['id'], request['recordSet']['id'])
     expect(request['recordSet']['ownerGroupId']).to eq(first_group['id'])
   end
